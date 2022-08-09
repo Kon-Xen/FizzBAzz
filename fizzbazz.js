06bd52233f6bfe13059633524ad87ca69e22df93
Konstantinos <kon.xen@gmail.com>
2022-08-09 10:56:14 +0100

    now using modulus as originally intended. more basic conditions added for 7, 11, 13

--- fizzbazz.js
+++ fizzbazz.js
@@ -1,16 +1,20 @@
+for ( let i = 1; i <= 100; i++ ){
 
-for (let i = 1; i <= 100; i++){
-
-    if (  Number.isInteger(  i / 3 ) && Number.isInteger(  i / 5 )  ) {
-        console.log( "FizzBuzz" );
-    } else if ( Number.isInteger(  i / 3 ) ) {
+    if (  i % 3 == 0 && i % 5 == 0   ) {
+        console.log("FizzBuzz");
+    } else if ( i % 3 == 0 ) {
         console.log( "Fizz" );
-    } else if ( Number.isInteger(i / 5 ) ) {
+    } else if ( i % 5 == 0 ) {
         console.log( "Buzz" );
+    } else if ( i % 7 == 0) {
+        console.log("Bang");
+    } else if (i % 11 == 0 ){
+        console.log("Bong");
+    }else if( i % 13 == 0 ) {
+        console.log("Fezz");
     } else {
         console.log(i);
     }
-        
-    
+
 }
 
